d7051fe6ee235f7eec77fe3c4c4525ed7172e061
truewayy <dldudwls888@gmail.com>
2023-04-30 18:57:47 +0900

    fix: 소셜 로그인 버튼 수정(아이콘, 클릭이벤트 추가)

--- components/Login/Button.tsx
+++ components/Login/Button.tsx
@@ -1,44 +1,49 @@
 import { Button } from '@chakra-ui/react';
 
+import ROUTES from '@/constants/routes';
+import { GoogleIcon, KakaoIcon } from '@/public/icon';
+
 export interface LoginButtonProps {
-  type: 'kakao' | 'naver' | 'apple' | 'google';
+  type: 'kakao' | 'google';
 }
 
 const login = {
   kakao: {
     msg: '카카오로 시작하기',
-    color: '#FFF',
+    color: '#000000',
     bgColor: '#FFC736',
-  },
-  naver: {
-    msg: '네이버로 시작하기',
-    color: '#FFF',
-    bgColor: '#26C035',
-  },
-  apple: {
-    msg: '애플로 시작하기',
-    color: '#484644',
-    bgColor: '#FFF',
+    bg: <KakaoIcon w={21} h={21} pos="absolute" left="26px" />,
+    handleClick: () => {
+      window.location.href = ROUTES.KAKAO_LOGIN;
+    },
   },
   google: {
     msg: '구글로 시작하기',
-    color: '#484644',
+    color: '#000000',
     bgColor: '#DADAE7',
+    bg: <GoogleIcon w={21} h={21} pos="absolute" left="26px" />,
+    handleClick: () => {
+      window.location.href = ROUTES.GOOGLE_LOGIN;
+    },
   },
 };
 
 const LoginButton = ({ type }: LoginButtonProps) => (
   <Button
     variant="outline"
+    pos="relative"
     w="100%"
     p="24px"
     borderRadius="8px"
     fontSize="16px"
-    fontWeight="500"
+    fontWeight="400"
     color={login[type].color}
     bgColor={login[type].bgColor}
+    onClick={login[type].handleClick}
+    _hover={{ bgColor: login[type].bgColor }}
   >
     {login[type].msg}
+    {login[type].bg}
   </Button>
 );
 
