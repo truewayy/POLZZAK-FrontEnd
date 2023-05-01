c477b9edc262b6e0b64146c7dc351346f33e8345
truewayy <dldudwls888@gmail.com>
2023-05-01 21:58:37 +0900

    refactor: 로그인 버튼 컴포넌트 VAC 패턴 적용

--- components/Login/Button.tsx
+++ components/Login/Button.tsx
@@ -3,48 +3,73 @@ import { Button } from '@chakra-ui/react';
 import ROUTES from '@/constants/routes';
 import { GoogleIcon, KakaoIcon } from '@/public/icon';
 
+type LoginType = 'kakao' | 'google';
+
 export interface LoginButtonProps {
-  type: 'kakao' | 'google';
+  type: LoginType;
 }
 
-const login = {
-  kakao: {
-    msg: '카카오로 시작하기',
-    color: '#000000',
-    bgColor: '#FFC736',
-    bg: <KakaoIcon w={21} h={21} pos="absolute" left="26px" />,
-    handleClick: () => {
-      window.location.href = ROUTES.KAKAO_LOGIN;
-    },
-  },
-  google: {
-    msg: '구글로 시작하기',
-    color: '#000000',
-    bgColor: '#DADAE7',
-    bg: <GoogleIcon w={21} h={21} pos="absolute" left="26px" />,
-    handleClick: () => {
-      window.location.href = ROUTES.GOOGLE_LOGIN;
-    },
-  },
+const LoginButton = ({ type }: LoginButtonProps) => {
+  const handleClick = () => {
+    switch (type) {
+      case 'kakao':
+        window.location.href = ROUTES.KAKAO_LOGIN;
+        break;
+      case 'google':
+        window.location.href = ROUTES.GOOGLE_LOGIN;
+        break;
+      default:
+        break;
+    }
+  };
+
+  const LoginButtonVAProps: LoginButtonVAProps = {
+    type,
+    handleClick,
+  };
+
+  return <LoginButtonView {...LoginButtonVAProps} />;
 };
 
-const LoginButton = ({ type }: LoginButtonProps) => (
-  <Button
-    variant="outline"
-    pos="relative"
-    w="100%"
-    p="24px"
-    borderRadius="8px"
-    fontSize="16px"
-    fontWeight="400"
-    color={login[type].color}
-    bgColor={login[type].bgColor}
-    onClick={login[type].handleClick}
-    _hover={{ bgColor: login[type].bgColor }}
-  >
-    {login[type].msg}
-    {login[type].bg}
-  </Button>
-);
+interface LoginButtonVAProps {
+  type: LoginType;
+  handleClick: () => void;
+}
+
+const LoginButtonView = ({ type, handleClick }: LoginButtonVAProps) => {
+  const loginStyle = {
+    kakao: {
+      msg: '카카오로 시작하기',
+      color: '#000000',
+      bgColor: '#FFC736',
+      bg: <KakaoIcon w={21} h={21} pos="absolute" left="26px" />,
+    },
+    google: {
+      msg: '구글로 시작하기',
+      color: '#000000',
+      bgColor: '#DADAE7',
+      bg: <GoogleIcon w={21} h={21} pos="absolute" left="26px" />,
+    },
+  };
+
+  return (
+    <Button
+      variant="outline"
+      pos="relative"
+      w="100%"
+      p="24px"
+      borderRadius="8px"
+      fontSize="16px"
+      fontWeight="400"
+      color={loginStyle[type].color}
+      bgColor={loginStyle[type].bgColor}
+      onClick={handleClick}
+      _hover={{ bgColor: loginStyle[type].bgColor }}
+    >
+      {loginStyle[type].msg}
+      {loginStyle[type].bg}
+    </Button>
+  );
+};
 
 export default LoginButton;
