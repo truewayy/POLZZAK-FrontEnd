55bb279cbe7c2168f91606164d05f46546ac23b6
truewayy <dldudwls888@gmail.com>
2023-05-10 18:42:26 +0900

    fix: 회원가입 후 온보딩 페이지로 라우팅

--- constants/routes.ts
+++ constants/routes.ts
@@ -16,6 +16,10 @@ const ROUTES = {
     PROFILE: '/oauth/signup/profile',
   },
   MAIN: '/main',
+  ON_BOARDING: {
+    PARENT: '/onboarding/parent',
+    KID: '/onboarding/kid',
+  },
   KAKAO_LOGIN: `https://kauth.kakao.com/oauth/authorize?client_id=${clientId.kakao}&redirect_uri=${redirectUri.kakao}&response_type=code`,
   GOOGLE_LOGIN: `https://accounts.google.com/o/oauth2/v2/auth?client_id=${clientId.google}&redirect_uri=${redirectUri.google}&response_type=code&scope=https://www.googleapis.com/auth/userinfo.profile`,
 };
