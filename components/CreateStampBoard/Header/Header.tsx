ed3abc55fa15f7a7408b9b4e1448fed8eca03472
truewayy <dldudwls888@gmail.com>
2023-05-16 16:07:50 +0900

    feat: 도장판 생성 페이지 헤더 작업 - submit 이베트

--- components/CreateStampBoard/Header/Header.tsx
+++ components/CreateStampBoard/Header/Header.tsx
@@ -1,20 +1,23 @@
 import { useRouter } from 'next/router';
+import { useFormContext } from 'react-hook-form';
 
 import HeaderView from './HeaderView';
 
 const Header = () => {
+  const { handleSubmit } = useFormContext();
   const { back } = useRouter();
 
   const handleClickBack = () => {
     back();
   };
 
-  const handleClickRegister = () => {
-    console.log('등록');
+  const handleClickRegister = (data: Object) => {
+    console.log(data);
   };
 
   const HeaderVAProps = {
     handleClickBack,
+    handleSubmit,
     handleClickRegister,
   };
 
--- components/CreateStampBoard/Header/HeaderView.tsx
+++ components/CreateStampBoard/Header/HeaderView.tsx
@@ -1,41 +1,46 @@
-import { Flex, Text } from '@chakra-ui/react';
+import { Box, Flex, Text } from '@chakra-ui/react';
+import { FieldValues, UseFormHandleSubmit } from 'react-hook-form';
 
 import { BackIcon } from '@/public/icon';
 
 interface HeaderVAProps {
   handleClickBack: () => void;
-  handleClickRegister: () => void;
+  handleSubmit: UseFormHandleSubmit<FieldValues>;
+  handleClickRegister: (data: Object) => void;
 }
 
 const HeaderView = ({
   handleClickBack,
+  handleSubmit,
   handleClickRegister,
 }: HeaderVAProps) => (
-  <Flex
-    w="100%"
-    h="44px"
-    justify="space-between"
-    alignItems="center"
-    pos="relative"
-  >
-    <BackIcon w={4} h={5} onClick={handleClickBack} />
-    <Text
-      layerStyle="subtitle1"
-      pos="absolute"
-      top="50%"
-      left="50%"
-      transform="translate(-50%, -50%)"
+  <Box w="100%" pos="fixed" top={0} left={0} p="0 5%" bg="white" zIndex={99}>
+    <Flex
+      w="100%"
+      h="44px"
+      justify="space-between"
+      alignItems="center"
+      pos="relative"
     >
-      도장판 생성
-    </Text>
-    <Text
-      layerStyle="subtitle3"
-      color="polzzak.default"
-      onClick={handleClickRegister}
-    >
-      등록
-    </Text>
-  </Flex>
+      <BackIcon w={4} h={5} onClick={handleClickBack} />
+      <Text
+        layerStyle="subtitle1"
+        pos="absolute"
+        top="50%"
+        left="50%"
+        transform="translate(-50%, -50%)"
+      >
+        도장판 생성
+      </Text>
+      <Text
+        layerStyle="subtitle3"
+        color="polzzak.default"
+        onClick={handleSubmit(handleClickRegister)}
+      >
+        등록
+      </Text>
+    </Flex>
+  </Box>
 );
 
 export default HeaderView;
