5a6fe1bfaf3ed677abfdc8aae3a51f22f79610bb
truewayy <dldudwls888@gmail.com>
2023-05-16 16:10:18 +0900

    feat: 도장판 생성 페이지 틀 작업

--- pages/create/stampboard/index.tsx
+++ pages/create/stampboard/index.tsx
@@ -1,12 +1,12 @@
 import { VStack } from '@chakra-ui/react';
 
 import SEO from '@/components/Common/SEO';
-import Header from '@/components/CreateStampBoard/Header/Header';
+import CreateForm from '@/components/CreateStampBoard/CreateForm/CreateForm';
 
 const CreateStampBoard = () => (
   <VStack p="0 5%">
     <SEO title="폴짝! | 도장판 생성" />
-    <Header />
+    <CreateForm />
   </VStack>
 );
 
