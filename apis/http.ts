cecd81abd75ae81a8097e8924827499d1bb25607
truewayy <dldudwls888@gmail.com>
2023-05-27 15:17:10 +0900

    feat: 로그인 이후 헤더에 토큰 추가

--- apis/http.ts
+++ apis/http.ts
@@ -1,4 +1,8 @@
+/* eslint-disable no-param-reassign */
 import axios, { AxiosInstance } from 'axios';
+
+import { TOKEN_KEY } from '@/constants/auth';
+import { getLocalStorage } from '@/utils/storage';
 
 // API 주소
 const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL;
@@ -6,7 +10,14 @@ const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL;
 // axios 인스턴스에 인터셉터를 추가하는 함수
 const setInterceptor = (instance: AxiosInstance) => {
   instance.interceptors.request.use(
-    (config) => config,
+    (config) => {
+      // 헤더에 토큰 추가
+      const accessToken = getLocalStorage(TOKEN_KEY);
+      if (accessToken) {
+        config.headers.Authorization = `Bearer ${accessToken}`;
+      }
+      return config;
+    },
     (error) => Promise.reject(error)
   );
 
