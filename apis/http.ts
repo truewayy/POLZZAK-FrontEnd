3b0e0813903747d341464409429adb1a434b7044
truewayy <dldudwls888@gmail.com>
2023-06-07 16:41:32 +0900

    fix: 전체 credential 추가

--- apis/http.ts
+++ apis/http.ts
@@ -6,6 +6,8 @@ import { getLocalStorage } from '@/utils/storage';
 
 // API 주소
 const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL;
+const googleAuth = 'https://oauth2.googleapis.com';
+const kakaoAuth = 'https://kauth.kakao.com';
 
 // axios 인스턴스에 인터셉터를 추가하는 함수
 const setInterceptor = (instance: AxiosInstance) => {
@@ -14,6 +16,12 @@ const setInterceptor = (instance: AxiosInstance) => {
       // 헤더에 토큰 추가
       const accessToken = getLocalStorage(TOKEN_KEY);
       if (accessToken) {
+        if (
+          !config.url?.includes(googleAuth) &&
+          !config.url?.includes(kakaoAuth)
+        ) {
+          config.withCredentials = true;
+        }
         config.headers.Authorization = `Bearer eyJhbGciOiJIUzM4NCJ9.eyJzdWIiOiJHT09HTEVfMTExNDk2OTMxOTY0MDgyMjEwNDY4Iiwicm9sZSI6IlJPTEVfVVNFUiIsImlhdCI6MTY4NjExNjcwMywiZXhwIjoxNjg2MTE2NzA0fQ.pqvsNrdxESxhKD_veOPWDAOI-b3-qXsBzV-ftayDBaQa5wcYOvxXQ-vmrqhyQWnN`;
       }
       return config;
