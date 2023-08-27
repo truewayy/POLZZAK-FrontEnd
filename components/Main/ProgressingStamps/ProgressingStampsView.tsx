7c7f211cc7ebc2ebf4d06249be074297bc1514b1
truewayy <dldudwls888@gmail.com>
2023-08-27 17:16:46 +0900

    fix: 개별 필터 도장판 없을 시 UI 적용

--- components/Main/ProgressingStamps/ProgressingStampsView.tsx
+++ components/Main/ProgressingStamps/ProgressingStampsView.tsx
@@ -1,6 +1,6 @@
 import 'swiper/css/pagination';
 
-import { VStack } from '@chakra-ui/react';
+import { Text, VStack } from '@chakra-ui/react';
 
 import { StampboardListData } from '@/apis/stamp';
 import PullToRefresh from '@/components/Common/PullToRefresh/PullToRefresh';
@@ -22,26 +22,47 @@ const ProgressingStampsView = ({
   <PullToRefresh onRefresh={handleRefresh}>
     {filter !== '전체' ? (
       <VStack w="100%" p="0 5%" spacing="20px">
-        {cards?.[0]?.stampBoardSummaries.map(
-          ({
-            stampBoardId,
-            name,
-            currentStampCount,
-            goalStampCount,
-            missionRequestCount,
-            reward,
-            status,
-          }) => (
-            <Card
-              key={stampBoardId}
-              stampBoardId={stampBoardId}
-              name={name}
-              currentStampCount={currentStampCount}
-              goalStampCount={goalStampCount}
-              missionRequestCount={missionRequestCount}
-              reward={reward}
-              status={status}
-            />
+        {cards?.[0]?.stampBoardSummaries.length === 0 ? (
+          <VStack
+            w="100%"
+            m="0 7%"
+            mb="30px"
+            bg="white"
+            h="410px"
+            border="1px dashed #DADAE7"
+            borderRadius="8px"
+            justifyContent="center"
+          >
+            <Text layerStyle="body14Md" textAlign="center" color="gray.700">
+              <Text as="span" layerStyle="body14Bd">
+                {filter}
+              </Text>
+              님과
+              <br /> 진행 중인 도장판이 없어요
+            </Text>
+          </VStack>
+        ) : (
+          cards?.[0]?.stampBoardSummaries.map(
+            ({
+              stampBoardId,
+              name,
+              currentStampCount,
+              goalStampCount,
+              missionRequestCount,
+              reward,
+              status,
+            }) => (
+              <Card
+                key={stampBoardId}
+                stampBoardId={stampBoardId}
+                name={name}
+                currentStampCount={currentStampCount}
+                goalStampCount={goalStampCount}
+                missionRequestCount={missionRequestCount}
+                reward={reward}
+                status={status}
+              />
+            )
           )
         )}
       </VStack>
